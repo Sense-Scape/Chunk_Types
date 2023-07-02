54863fd593782dac9853b16f69bf2db73eac6507
Grabt234 <gnorrie12@gmail.com>
2023-07-02 19:19:09 +0200

    Updating BaseChunk tests

--- include_tests/BaseChunkTests.h
+++ include_tests/BaseChunkTests.h
@@ -8,52 +8,52 @@ TEST_CASE("BaseChunk Test") {
 
     // Lets just start by creating a vanilla BaseChunk
     BaseChunk BaseChunkTestClass;
+    BaseChunk BaseChunkTestClassCopy_0;
+
+    // We can also check the basic serialisation functionality
+    auto pvcBaseChunkBytes = BaseChunkTestClass.Serialise();
+    BaseChunkTestClassCopy_0.Deserialise(pvcBaseChunkBytes);
 
 
-    SUBCASE("Checking base chunk default constructor") {
+    SUBCASE("Checking base chunk after using default constructor") {
         // And then that its type is basechunk
         CHECK(BaseChunkTestClass.GetChunkType() == ChunkType::ChunkBase);
         // Then lets ensure that the size of the data contained is 2 bytes
         CHECK(BaseChunkTestClass.GetSize() == 2);
+        // lets check for equality to see if serialisation works
+        CHECK(BaseChunkTestClass.IsEqual(BaseChunkTestClassCopy_0));
     }
 
 
     // Lets now give the device a unique identifier and see if the size if correct
     std::vector<uint8_t> vu8Indentidier = { 255, 255, 255 };
     BaseChunkTestClass.SetSourceIdentifier(vu8Indentidier);
+    // We can also check serialisation again
+    pvcBaseChunkBytes = BaseChunkTestClass.Serialise();
+    BaseChunkTestClassCopy_0.Deserialise(pvcBaseChunkBytes);
 
 
-    SUBCASE("Checking base set set source identifier (to verify size changes)") {
+    SUBCASE("Checking source identifier usage - size, member value and serialisation") {
         // Then lets ensure that the size of the data contained is 2 bytes
         CHECK(BaseChunkTestClass.GetSize() == 5);
         // We can also check if the vector was set correctly
         CHECK(BaseChunkTestClass.GetSourceIdentifier() == vu8Indentidier);
+        // lets check for equality to see if serialisation works
+        CHECK(BaseChunkTestClass.IsEqual(BaseChunkTestClassCopy_0));
     }
 
-    // Checking copy constructors
-    BaseChunk ReferenceCopyBaseChunkTestClass(BaseChunkTestClass);
-    std::shared_ptr<BaseChunk> pBaseChunkTestClass = std::make_shared<BaseChunk>(BaseChunkTestClass);
-    auto pPointerCopyBaseChunkTestClass(pBaseChunkTestClass);
+
+    // We can also go ahead and check if the copy constructor works
+    BaseChunk BaseChunkTestClassCopy_1(BaseChunkTestClass);
+    std::shared_ptr<BaseChunk> pBaseChunkTestClassCopy_2 = std::make_shared<BaseChunk>(BaseChunkTestClass);
 
 
-    SUBCASE("Checking reference copy constructors") {
-        CHECK(BaseChunkTestClass.IsEqual(ReferenceCopyBaseChunkTestClass));
-        CHECK(BaseChunkTestClass.IsEqual(*pPointerCopyBaseChunkTestClass));
+    SUBCASE("Checking copy constructors using reference and pointer") {
+        // checking copy by reference
+        CHECK(BaseChunkTestClass.IsEqual(BaseChunkTestClassCopy_1));
+        // and then by pointer
+        CHECK(BaseChunkTestClass.IsEqual(*pBaseChunkTestClassCopy_2));
     }
-
-    // Checking de.serialisation
-    BaseChunk DeserialisedBaseChunkTestClass;
-    std::shared_ptr<std::vector<char>> pBytes = BaseChunkTestClass.Serialise();
-    DeserialisedBaseChunkTestClass.Deserialise(pBytes);
-
-
-    SUBCASE("Checking De/Serialisation functionality") {
-        // Verify identifier was copied correctly
-        CHECK(BaseChunkTestClass.GetSourceIdentifier() == vu8Indentidier);
-        // Then check if equal
-        CHECK(BaseChunkTestClass.IsEqual(DeserialisedBaseChunkTestClass));
-    }
-
 }
 
 #endif
